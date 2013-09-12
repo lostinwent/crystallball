8e71446d61bca3c7634d027c0e782c26022722e3
lostinwent <stein@steinw.com>
2013-09-11 23:46:11 -0400

    random the predictionArray

--- CrystalBall/ViewController.m
+++ CrystalBall/ViewController.m
@@ -19,7 +19,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.predictionArray = [[NSArray alloc] initWithObjects:@"It is certain", @"It is decidedly so", nil];      //make predictionArray a property self. is the view controller, predictionArray is the property of the view controller
+    self.predictionArray = [[NSArray alloc] initWithObjects:@"It is certain", @"It is decidedly so", @"All signs say YES", @"The stars are not aligned", @"My answer is No", @"It is doubtful", @"Better not tell you now", @"Cncentrate and ask again", @"Maybe Yes", nil];      //make predictionArray a property self. is the view controller, predictionArray is the property of the view controller
     
 	// Do any additional setup after loading the view, typically from a nib.
 }
@@ -30,11 +30,12 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)buttonPressed:(UIButton *)sender {
-    NSString *myString = @"My name is Yoooo";
-    [myString length];
-    [myString uppercaseString];
-    
-    self.predictionLabel.text = [self.predictionArray objectAtIndex:0];
+- (IBAction)buttonPressed:(UIButton *)sender {  //buttonPressed is a method, method belongs to a class
+    NSUInteger index = arc4random_uniform(self.predictionArray.count); // NSUInteger is not a class but a type definition, arc4random_uniform is a function
+//    NSString *myString = @"My name is Yoooo";
+//    [myString length];
+//    [myString uppercaseString];
+    NSLog(@"%lu", (unsigned long)index);
+    self.predictionLabel.text = [self.predictionArray objectAtIndex:index];
 }
 @end
